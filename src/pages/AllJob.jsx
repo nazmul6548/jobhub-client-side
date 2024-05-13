303d150ff826c65ea638036041feec63e2f53a7f
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-05-13 21:38:58 +0600

    add search input

--- src/pages/AllJob.jsx
+++ src/pages/AllJob.jsx
@@ -35,10 +35,30 @@ const AllJob = () => {
         return(data)
       }
       if (isLoading) return <p className="text-center flex justify-center">data is loading......</p>
+      const handleSearch =e=>{
+        e.preventDefault();
+      }
     return (
         
         
         <div className="overflow-x-auto pb-8 mt-24">
+            <form onSubmit={handleSearch}>
+            <div className='flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300'>
+              <input
+                className='px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent'
+                type='text'
+                onChange={e => setSearchText(e.target.value)}
+                // value={searchText}
+                name='search'
+                placeholder='Enter Job Title'
+                aria-label='Enter Job Title'
+              />
+
+              <button className='px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none'>
+                Search
+              </button>
+            </div>
+          </form>
             <div>
             <motion.div style={{ scaleX: scrollYProgress }} />  
             </div>
